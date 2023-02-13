3177420e1a6bf2590ae790a1f23badee0a22dec0
bleaknarrative <124403331+bleaknarrative@users.noreply.github.com>
2023-02-13 19:47:44 +0100

    marseille

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function showClients() {
+  var x = document.getElementById("clients");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function showSkills() {
+  var x = document.getElementById("skills");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function showInfo() {
+  var x = document.getElementById("info");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
